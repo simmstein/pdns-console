474d1eed2f1e278cb43d6920583569910d71f26e
Simon Vieille <simon@deblan.fr>
2015-02-17 01:20:26 +0100

    Schema propel

---
+++ src/Deblan/PowerDNS/Model/ZoneQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Deblan\PowerDNS\Model;
+
+use Deblan\PowerDNS\Model\Base\ZoneQuery as BaseZoneQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'zone' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class ZoneQuery extends BaseZoneQuery
+{
+
+}
