c24c3f6e47e561cd4af9fc6a15530136986d7a7a
Simon Vieille <simon@deblan.fr>
2015-02-17 01:18:05 +0100

    Application console

---
+++ src/Deblan/Console/Application.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Deblan\Console;
+
+use ReflectionException;
+use ReflectionClass;
+use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Finder\Finder;
+
+class Application extends BaseApplication
+{
+    protected $commandsPaths = array();
+
+    public function addCommandsPath($path, $namespace)
+    {
+        $this->commandsPaths[$path] = trim($namespace, '\\');
+
+        return $this;
+    }
+
+    public function chdir($directory)
+    {
+        chdir($directory);
+
+        return $this;
+    }
+
+    public function loadCommands()
+    {
+        foreach ($this->commandsPaths as $path => $namespace) {
+            $finder = new Finder();
+            $finder->name('*Command.php')->in($path);
+
+            foreach ($finder as $file) {
+                $className = $namespace.'\\'.str_replace('.php', '', $file->getFilename());
+
+                try {
+                    $reflexion = new ReflectionClass($className);
+
+                    if ($reflexion->isInstantiable()) {
+                        $command = new $className();
+
+                        $this->addCommands(array(
+                            $command,
+                        ));
+                    }
+                } catch (ReflectionException $e) {
+
+                }
+            }
+        }
+
+        return $this;
+    }
+}
