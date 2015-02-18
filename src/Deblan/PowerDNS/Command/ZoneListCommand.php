1aa79400548bcc52d91f62e2e44bd2bba395b1f9
Simon Vieille <simon@deblan.fr>
2015-02-18 13:54:54 +0100

    Removing auto-generated comments

--- src/Deblan/PowerDNS/Command/ZoneListCommand.php
+++ src/Deblan/PowerDNS/Command/ZoneListCommand.php
@@ -5,7 +5,6 @@ namespace Deblan\PowerDNS\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Command\Command;
 use Deblan\PowerDNS\Model\ZoneQuery;
 
@@ -13,12 +12,11 @@ class ZoneListCommand extends AbstractZoneCommand
 {
     protected function configure()
     {
-		parent::configure();
+        parent::configure();
 
         $this
             ->setName('zone:list')
             ->setDescription('List DNS zones')
-            // ->addArgument('foo', InputArgument::OPTIONAL, '')
             ->addOption('name', null, InputOption::VALUE_REQUIRED, '')
             ->setHelp("The <info>%command.name%</info> ");
     }
@@ -27,12 +25,7 @@ class ZoneListCommand extends AbstractZoneCommand
     {
         parent::execute($input, $output);
 
-        // $this->getContainer()->get('foo.bar');
-        // $output->writeln(sprintf('<comment>%s</comment> bar.', $example));
-        // $input->getArgument('foo');
-        // $input->getOption('bar');
-
-		$query = $this->getZoneQuery();
+        $query = $this->getZoneQuery();
 
         $zones = $query->find();
 
